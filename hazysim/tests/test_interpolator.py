82169c9d90d31f59602cba18850ddd091ee7c60c
Christopher Aberger <christopheraberger@Christophers-MacBook-Pro-2.local>
2018-06-08 12:48:14 -0700

    Update tests.

--- hazysim/tests/test_interpolator.py
+++ hazysim/tests/test_interpolator.py
@@ -3,7 +3,38 @@ import hazysim as sim
 import torch
 
 def test_sin():
-    print("testing sin")
+    a_interp = torch.tensor([1.0,0.0,-1.0,2.0], requires_grad=True)
+    a_actual = torch.tensor([1.0,0.0,-1.0,2.0], requires_grad=True)
+
+    sin = torch.sin
+    isin = sim.nn.Interpolator(torch.sin)
+
+    """
+    Grid the points using an adaptive method.
+    Will adapt points in the range [start, end].
+    Each subinterval [x(i),x(i+1)] is either <= hmin in length or has the property 
+    that at its midpoint m, |f(m) - L(m)| <= delta where L(x) is the line that 
+    connects (x(i),y(i)) and (x(i+1),y(i+1)).
+    """
+    isin.adapt(start = -8, end = 10, delta = 0.01, hmin = 0.01)
+    isin.chunk(min = -8, max = 10, num_points = 100)
+    print(isin)
+    sin_result = isin(a_interp)
+    sin_result.sum().backward()
+    print(sin_result)
+    print(a_interp.grad)
+
+    print("Forward Acutal")
+    actual = torch.sin(a_actual)
+    print(actual)
+    print("Backward Actual")
+    actual.sum().backward()
+    print(a_actual.grad)
+
+    print(torch.abs(actual - sin_result).sum().item())
+    assert(torch.abs(actual - sin_result).sum().item() <= 0.0051)
+    print(torch.abs(a_actual.grad - a_interp.grad).sum().item())
+    assert(torch.abs(a_actual.grad - a_interp.grad).sum().item() <= 0.0065)
 
 def test_exp():
     a_interp = torch.tensor([-90.0,-10,10,75], requires_grad=True)
